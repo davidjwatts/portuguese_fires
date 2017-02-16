09dc502a719fdabec7d0152f4ab02f6ac8d6a47d
David J Watts <davidjeremywatts@gmail.com>
2017-02-15 18:50:16 -0700

    Add to data story

--- utils.py
+++ utils.py
@@ -5,17 +5,18 @@ PATH = 'forestfires.csv'
 LCASE_DAY_ABRV_DICT = {'sun':1,'mon':2,'tue':3,'wed':4,'thu':5,'fri':6,'sat':7}
 
 def short_lcase_month_to_int(series):
-    series = series.map(lambda x: x.title())
-    series = pd.to_datetime(series, format='%b')
-    series = series.map(lambda x: x.month)
+    int_series = series.map(lambda x: x.title())
+    int_series = pd.to_datetime(int_series, format='%b')
+    int_series = int_series.map(lambda x: x.month)
+    return int_series
 
 def wrangle():
-    #read csv to DataFrame
-    df = pd.read_csv(PATH, sep=',', header=0)
-    #Convert month abbreviations to integers
-    short_lcase_month_to_int(df['month'])
-    #Convert day abbreviations to integers
-    df['day'] = df['day'].map(lambda x:LCASE_DAY_ABRV_DICT[x])
-    #Create new ln of area column
-    df['log(area)'] = np.log(df['area']+1)
-    return df
+	#read csv to DataFrame
+	df = pd.read_csv(PATH, sep=',', header=0)
+	#Convert month abbreviations to integers
+	df['month'] = short_lcase_month_to_int(df['month'])
+	#Convert day abbreviations to integers
+	df['day'] = df['day'].map(lambda x:LCASE_DAY_ABRV_DICT[x])
+	#Create new ln of area column
+	df['log(area)'] = np.log(df['area']+1)
+	return df
