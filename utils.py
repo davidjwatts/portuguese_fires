e4200073c425ec887dfdd0396abcad3b86554178
David J Watts <davidjeremywatts@gmail.com>
2017-02-17 11:10:17 -0700

    add titles and area=0 boolean column

--- utils.py
+++ utils.py
@@ -2,21 +2,29 @@ import pandas as pd
 import numpy as np
 
 PATH = 'forestfires.csv'
+
+# can use this dictionary to map a Series of the abbreviations to integers
 LCASE_DAY_ABRV_DICT = {'sun':1,'mon':2,'tue':3,'wed':4,'thu':5,'fri':6,'sat':7}
 
 def short_lcase_month_to_int(series):
+    """
+    convert Series of lower case short abbreviations of month
+    names to Series of corresponding integers.
+    """
     int_series = series.map(lambda x: x.title())
     int_series = pd.to_datetime(int_series, format='%b')
     int_series = int_series.map(lambda x: x.month)
     return int_series
 
 def wrangle():
-	#read csv to DataFrame
+	"""
+	reads csv from dataframe, converts month and day short abbreviations
+	to integers, creates 'log(area)' column and 'small' boolean column	
+	"""
+	
 	df = pd.read_csv(PATH, sep=',', header=0)
-	#Convert month abbreviations to integers
 	df['month'] = short_lcase_month_to_int(df['month'])
-	#Convert day abbreviations to integers
 	df['day'] = df['day'].map(lambda x:LCASE_DAY_ABRV_DICT[x])
-	#Create new ln of area column
 	df['log(area)'] = np.log(df['area']+1)
+	df['area=0'] = df['area']==0
 	return df
